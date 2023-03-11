3ecc839c4cb84672152a68ddca4d964def4cb0b8
OlhaNekrylova <nekrylovaurist@gmail.com>
2023-03-11 21:42:22 +0200

    add redux

--- src/UserRoutes.jsx
+++ src/UserRoutes.jsx
@@ -1,8 +1,12 @@
 import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import PublicRoute from "./modules/PublicRoute/PublicRoute";
+import PrivateRoute from './modules/PrivateRoute/PrivateRoute';
+
 const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
-const SearchPage = lazy(() => import("./pages/SearchPage/SearchPage"));
+const RegisterPage = lazy(() => import("./pages/HomePage/HomePage"));
+const LoginPage = lazy(() => import("./pages/HomePage/HomePage"));
 const ContactsPage = lazy(() => import("./pages/ContactsPage/ContactsPage"));
 const NotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
 
@@ -11,8 +15,13 @@ const UserRoutes = () => {
         <Suspense fallback={<p>...loading</p>}>
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/search" element={<SearchPage />} />
-                <Route path="/contacts" element={<ContactsPage />} />
+                <Route element={<PublicRoute />}>
+                    <Route path="/register" element={<RegisterPage />} />
+                    <Route path="/login" element={<LoginPage />} />
+                </Route>
+                <Route element={<PrivateRoute />}>
+                    <Route path="/contacts" element={<ContactsPage />} />
+                </Route>
                 <Route path="*" element={<NotFoundPage />} />
             </Routes>
         </Suspense>
--- src/modules/Navigation/Navigation.jsx
+++ src/modules/Navigation/Navigation.jsx
@@ -1,9 +1,18 @@
 import { NavLink } from "react-router-dom";
-import links from "../items";
+import { useSelector } from "react-redux";
+
+import { isUserLogin } from "../../redux/auth/auth-selectors";
+
+import NavbarAuth from "./NavbarAuth/NavbarAuth";
+import NavbarUser from "./NavbarUser/NavbarUser";
+
+import items from "../items";
 import styles from "./Navbar.module.css";
 
-const Navbar = ()=> {
-    const elements = links.map(({id, text, link}) => 
+const Navigation = ()=> {
+    const isLogin = useSelector(isUserLogin);
+    const filteredItems = !isLogin ? items.filter(item => !item.private) : items;
+    const elements = filteredItems.map(({id, text, link}) => 
         <li key={id}>
         <NavLink className={styles.link} to={link}>
             {text}
@@ -19,5 +28,5 @@ const Navbar = ()=> {
     )
 }
 
-export default Navbar;
+export default Navigation;
 
